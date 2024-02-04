b1908fdd6a730e447102b233c9571c1164ba6508
jeanriffel <jeanriffel1@gmail.com>
2024-02-04 15:04:22 -0300

    feat: adding mui-component

--- src/components/atoms/customButton/customButton.tsx
+++ src/components/atoms/customButton/customButton.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Button} from '@mui/material'
+import {Button, Typography} from '@mui/material'
 
 const CustomButton = (props: any) => {
   return (
-    <Button 
-      sx={{width: '248px', padding: '2px'}}
-      variant="contained"
-      onClick={props.onClick}    
-    >
-      {props.text}
-    </Button>
+    <>
+      <Button 
+        sx={{width: '248px', padding: '2px'}}
+        variant="contained"
+        onClick={props.onClick}    
+      >
+        <Typography>
+          {props.text}
+        </Typography>
+      </Button>
+    </>
   )
 }
 
