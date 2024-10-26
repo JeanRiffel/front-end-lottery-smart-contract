1947a3cc10fecf99a6a11a33191a1ce0147741eb
user <jeanriffel1@gmai.com>
2024-10-26 18:20:54 -0300

    refactor: adjusting code base

--- src/components/molecules/LotteryForm/LotteryForm.tsx
+++ src/components/molecules/LotteryForm/LotteryForm.tsx
@@ -1,85 +1,102 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect, useState, useContext} from 'react';
 import CustomField from '../../atoms/customField/customField';
 import CustomButton from '../../atoms/customButton/customButton';
 import CustomGrid from '../../atoms/customGrid/customGrid';
 import index from '../../pages/index.module.css';
+import axios from 'axios'
 
 import styles from './LotteryForm.module.scss'
-import BetContext from '@/contexts/BetContext'
 import { Box } from '@mui/material';
+
+interface Player {
+  address: string;
+  value: number;
+  
+}
 
 const LotteryForm: React.FC = (props: any)=> {
 
-  const { 
-    gambler, setGambler, 
-    betValue, setBetValue, 
-    betAmount, setBetAmount,
-    address, setAddress,
-    players, setPlayers
-  } = useContext(BetContext);
+  const [players, setPlayers] = useState<Player[]>([])
+  const [betValue, setBetValue] = useState<number>(0)
+  const [betAddress, setBetAddress] = useState<string>('')
+  const [winner, setWinner] = useState<string>('')
 
-  const handleChangeGambler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setGambler(e.target.value);
-  };
+  const handleBetValue = (e: React.ChangeEvent<HTMLInputElement>) => setBetValue(Number(e.target.value))
+  const handleBetAddress = (e: React.ChangeEvent<HTMLInputElement>) => setBetAddress(e.target.value)
+  const handlePlayer = ()=> {
+    const newPlayer: Player = {
+      address: betAddress,
+      value: betValue,
+    }
 
-  const handleChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBetValue(e.target.value);
-  };
-
-  const handleChangeAmount = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setBetAmount(e.target.value);
-  };
-
-  const handleChangeAddress = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setAddress(e.target.value);
-  };
-
-  const handleBet = () => {
-    const player = { gambler, betValue, betAmount, address }
-    
-    if (!gambler || !betValue || !betAmount || !address) return
-    
-    setPlayers((prevPlayers) => [...prevPlayers, player,]);
-  };
-
-  const handleClearPlayers = () => {
-    setPlayers([])
-  };
-
+    setPlayers([...players, newPlayer]);
+  }
   
+
+  const placeBet = async () => {
+      const inputData: Player[] = 
+        [
+          { 
+            address: "0xd57Ec36C833ae69Ba18f63AFB23707B7D43cF68C",
+            value: 4
+          },
+          {
+            address: "0xD3E0cdE4f7BE2e559D64e24950044C2BD1c998cd",
+            value: 5
+          }
+        ]
+      
+
+      const response = await axios.post('http://localhost:3001/lottery/place-bet', {
+        data: inputData,
+      });
+      console.log(response.data);
+  }
+
+
+  const getWinner = async () => {
+  
+      const response = await axios.get('http://localhost:3001/lottery/winner'); 
+      console.log(response.data);
+
+  }
+
+
   return (
     <div>      
         <Box className={styles.lotteryForm__fields}>
           <CustomField 
-            label="Gambler" 
-            onChange={handleChangeGambler} 
-          />  
-          <CustomField 
             label="Bet Value" 
-            onChange={handleChangeValue}
-          />                  
-          <CustomField 
-            label="Bet Amount"          
-            onChange={handleChangeAmount}
-          />
+            onChange={handleBetValue}
+          />            
           <CustomField 
             label="Account Address"          
-            onChange={handleChangeAddress}
+            onChange={handleBetAddress}
           />          
         </Box>
         <Box className={styles.lotteryForm__buttons}>
           <CustomButton
-            onClick={handleBet}
-            text='Place Bet'
+            onClick={handlePlayer}
+            text='Add Player'
           />          
           <CustomButton
-            onClick={handleClearPlayers}
+         
             text='Clear Players'
           />
+
+          <CustomButton
+          
+            text='Place Bet'
+            onClick={placeBet}
+          />
+
+          <CustomButton
+          
+          text='Get Winner'
+          onClick={getWinner}
+        />
         </Box>      
-        <Box className={styles.lotteryForm__grid}>
-          <CustomGrid players={players}/>
-        </Box>
+  
     </div>
   )  
 }
